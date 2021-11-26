a7304cb4dcd11d2c309832bbe4cc5e45516d7632
davi cruz <daviondejesus@gmail.com>
2021-11-26 09:59:21 -0300

    init

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+    require __DIR__ . '/vendor/autoload.php';
+
+    use Source\Controllers\User;
+    use Psr\Http\Message\ResponseInterface;
+
+    $app = new \Slim\App([
+        'settings' => [
+            'displayErrorDetails' => true,
+        ],
+    ]);
+
+    $app->group('/auth', function () use ($app){
+        $app->post('/login', User::class . ":login");
+    });
+
+    $app->group('/products', function () use ($app) {
+        $app->get('/list', function (ResponseInterface $response){
+            return $response->withJson([
+                "data" => PRODUCTS,
+            ])->withStatus(200);
+        });
+    });
+
+    $app->run();
---
+++ source/Controllers/User.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace Source\Controllers;
+
+    require __DIR__ . '/../../vendor/autoload.php';
+    use Psr\Http\Message\RequestInterface;
+    use Psr\Http\Message\ResponseInterface;
+
+    final class User extends DataLayer
+    {
+        public function login(RequestInterface $request, ResponseInterface $response): ResponseInterface
+        {
+            $data = $request->getParsedBody();
+
+            if (!filter_var($data["email"], FILTER_VALIDATE_EMAIL))
+                return $response->withJson(["message" => "Email inválido, tente com outro!"])->withStatus(200);
+
+            $list_users = USERS;
+
+            foreach ($list_users as $user)
+            {
+                if($data['email'] == $user->email && $data['senha'] == $user->senha)
+                    return $response->withJson(["message" => "Success", "user" => $user])->withStatus(200);
+            }
+
+            return $response->withJson(["message" => "Email ou senha inválido"])->withStatus(200);
+        }
+    }

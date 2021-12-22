b4a1fbb532ba8c6cc410f7cecf071e56692daa7c
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2021-12-22 15:52:02 -0300

    Update Config.php

--- source/Config.php
+++ source/Config.php
@@ -3,70 +3,70 @@
     define("PRODUCTS",[
         [
             "name" => "Ank Headphone",
-            "price" => 1.980,
+            "price" => "1.980,00",
             "reviwers" => 30,
             "starts" => 4.3,
             "photo" => "https://images.unsplash.com/photo-1484704849700-f032a568e944?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhZHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
         ],
         [
             "name" => "Sony AirBeat",
-            "price" => 2.360,
+            "price" => "2.360,00",
             "reviwers" => 45,
             "starts" => 4.5,
             "photo" => "https://images.unsplash.com/photo-1618366712010-f4ae9c647dcb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aGVhZHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
         ],
         [
             "name" => "Apple AirMax",
-            "price" => 7.210,
+            "price" => "7.210,00",
             "reviwers" => 80,
             "starts" => 5,
             "photo" => "https://images.unsplash.com/photo-1613040809024-b4ef7ba99bc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aGVhZHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
         ],
         [
             "name" => "Beats Link",
-            "price" => 2.360,
+            "price" => "2.360,00",
             "reviwers" => 90,
             "starts" => 5,
             "photo" => "https://images.unsplash.com/photo-1545127398-14699f92334b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aGVhZHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
         ],
         [
             "name" => "Radio Beats",
-            "price" => 14.200,
+            "price" => "14.200,00",
             "reviwers" => 100,
             "starts" => 5,
             "photo" => "https://images.unsplash.com/photo-1481207801830-97f0f9a1337e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTR8fGhlYWRwaG9uZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
         ],
         [
             "name" => "Jbl Wireless",
-            "price" => 820,
+            "price" => "820,00",
             "reviwers" => 230,
             "starts" => 3.5,
             "photo" => "https://images.unsplash.com/photo-1579065560489-989b0cc394ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODV8fGhlYWRwaG9uZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
         ],
         [
             "name" => "Apple Airpods Pro",
-            "price" => 2.210,
+            "price" => "2.210,00",
             "reviwers" => 130,
             "starts" => 5,
             "photo" => "https://images.unsplash.com/photo-1572569511254-d8f925fe2cbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWlycG9kc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
         ],
         [
             "name" => "Apple Airpods 2",
-            "price" => 1.360,
+            "price" => "1.360,00",
             "reviwers" => 420,
             "starts" => 5,
             "photo" => "https://images.unsplash.com/photo-1600375226700-5ce19fbe1a6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGFpcnBvZHN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
         ],
         [
             "name" => "AKG Motion",
-            "price" => 9.200,
+            "price" => "9.200,00",
             "reviwers" => 100,
             "starts" => 5,
             "photo" => "https://images.unsplash.com/photo-1536656930057-9252abf296a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aGVhZHBob25lJTIwYmx1ZXRob290aHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
         ],
         [
             "name" => "DENO Bambu",
-            "price" => 3.789,
+            "price" => "3.789,00",
             "reviwers" => 30,
             "starts" => 4.5,
             "photo" => "https://images.unsplash.com/photo-1532778489370-ffc5d2095091?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGhlYWRwaG9uZSUyMGJsdWV0aG9vdGh8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
